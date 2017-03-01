086fe31b27639e58f8be932544b840e7ac732aea
UTPBryanPineda <bcamilo@utp.edu.co>
2017-03-01 10:08:09 -0500

    cambios

--- mpitaller1/sumavectores.sh
+++ mpitaller1/sumavectores.sh
@@ -2,8 +2,10 @@
 
 #SBATCH --job-name=sumavectores
 #SBATCH --output=res_mpi_sumavectores.out
-#SBATCH --ntasks=48
+#SBATCH --ntasks=3
+#SBATCH --nodes=3
 #SBATCH --time=10:00
 #SBATCH --mem-per-cpu=100
+#SBATCH --gres=gpu:1
 
 mpirun sumavectores
